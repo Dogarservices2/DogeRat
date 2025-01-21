9cefdcfa478a93c8bde9bf10232d26e68db6c0d1
Dogarservices2 <122605575+Dogarservices2@users.noreply.github.com>
2025-01-21 15:55:48 +0500

    Update server.js

--- APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
+++ APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '7804446450'
+const token = '7492169567:AAGKqpGW3r0bHscgiovwKFnzgfUinaJlhpI'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
